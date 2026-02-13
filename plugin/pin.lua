0911c5fd5828679467f1c82bc99fa49ac1df1fdf
qrikko <qrikko@gmail.com>
2026-02-13 21:58:32 +0100

    started over somewhat, new strategy using selection perhaps, but focusing a bit more towards using treesitter

--- plugin/pin.lua
+++ plugin/pin.lua
@@ -1,5 +1,4 @@
-local ns = vim.api.nvim_create_namespace('pinned_selection')
-local state = { extmark_id = nil, visible = false }
+local state = { visible = false }
 
 local function get_visual_selection()
     local start = vim.fn.getpos("'<'")
@@ -13,30 +12,37 @@ local function get_visual_selection()
     }
 end
 
-local function clear_pin()
-    if state.extmark_id then
-        vim.api.nvim_buf_del_extmark(0, ns, state.extmark_id)
-        state.extmark_id = nil
-        state.visible = false
-    end
-end
-
-local function pin_toggle()
-    if state.visible then
-        clear_pin()
-    else
+local function pin_selection()
         local sel = get_visual_selection()
-        local opts = {
-            hl_group = 'Visual',
-            end_row = sel.finish,
-            end_col = sel.finish,
-        }
-        state.extmark_id = vim.api.nvim_buf_set_extmark(0, ns, sel.start_line, sel.start_col, {
-            end_row = sel.end_line,
-            end_col = sel.end_col,
-            hl_group = "Visual",
-        })
-    end
 end
 
-vim.api.nvim_create_user_command('Pin', function() pin_toggle() end, { nargs = 0, range = true})
+local function pin_ts_node()
+    local ts_utils = require('nvim-treesitter.ts_utils')
+    local node = ts_utils.get_node_at_cursor()
+
+    if not node then return end
+
+    -- Get the actual text of the node
+    local bufnr = vim.api.nvim_get_current_buf()
+    local text = vim.treesitter.get_node_text(node, bufnr)
+    local lines = vim.split(text, '\n')
+
+    -- Create a scratch buffer for the popup
+    local float_buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(float_buf, 0, -1, false, lines)
+    vim.api.nvim_set_option_value('filetype', vim.bo.filetype, { buf = float_buf })
+
+    -- Open the window
+    vim.api.nvim_open_win(float_buf, false, {
+        relative = 'editor',
+        row = 5,
+        col = 5,
+        width = 60,
+        height = #lines + 2,
+        style = 'minimal',
+        border = 'rounded'
+    })
+end
+
+vim.api.nvim_create_user_command('PinSelection', function() pin_selection() end, { nargs = 0, range = true})
+vim.api.nvim_create_user_command('PinTSNode', pin_ts_node, {})
