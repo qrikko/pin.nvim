885a22cd8e97b338275e6849c20b2f653e43a4f5
qrikko <qrikko@gmail.com>
2026-02-13 20:21:02 +0100

    first version

--- plugin/pin.lua
+++ plugin/pin.lua
@@ -1,6 +1,34 @@
-local M = {}
+local ns = vim.api.nvim_create_namespace('pinned_selection')
+local state = { extmark_id = nil, visible = false }
 
-function M.setup()
-    --init
+local function get_visual_selection()
+    local sel = {}
+    sel.start = vim.fn.getpos("'<'")
+    sel.finish = vim.fn.getpos("'>'")
+    return sel
 end
-return M
+
+local function clear_pin()
+    if state.extmark_id then
+        vim.api.nvim_buf_del_extmark(0, ns, state.extmark_id)
+        state.extmark_id = nil
+        state.visible = false
+    end
+end
+
+local function pin_toggle()
+    if state.visible then
+        clear_pin()
+    else
+        local sel = get_visual_selection()
+        local opts = {
+            hl_group = 'Visual',
+            end_row = sel.finish,
+            end_col = sel.finish,
+        }
+        state.extmark_id = vim.api.nvim_buf_set_extmark(0, ns, sel.start, sel.finish, opts)
+        state.visible = true
+    end
+end
+
+vim.api.nvim_create_user_command('Pin', pin_toggle, {})
